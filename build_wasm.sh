e832b2f23f87b284995bb7b8c88f879331091f7e
Robert Masen <r@robertmasen.pizza>
2018-04-25 07:14:44 -0500

    create build script for wasm project

---
+++ build_wasm.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+cd crates/browser
+cargo +nightly build --target wasm32-unknown-unknown
+cd ../..
+wasm-bindgen ./target/wasm32-unknown-unknown/debug/wasm_tutorial_browser.wasm --browser --out-dir ./dist
+
+if [ ! -f ./wbch ]; then
+    echo "creating temp"
+    echo
+    CURDIR=$PWD
+    echo $CURDIR
+    TEMP=$(mktemp)
+    echo $TEMP
+    echo "removing temp file"
+    rm -r $TEMP
+    echo "cloning into temp folder"
+    git clone https://github.com/freemasen/wasm-chrome-hack $TEMP
+    echo "moving into temp directory"
+    cd $TEMP
+    echo "building project"
+    cargo build --release
+    echo "copying bin"
+    cp ./target/release/wbch $CURDIR
+    echo "move back to project folder"
+    cd $CURDIR
+    echo "removing temp dir"
+    rm -rf $TEMP
+    echo "chmod +x ing bin"
+    chmod +x ./wbch
+fi
+echo "modifying file"
+./wbch ./dist/wasm_tutorial_browser.js ./ts/wasm_tutorial_browser.js
+rm ./dist/wasm_tutorial_browser.js
