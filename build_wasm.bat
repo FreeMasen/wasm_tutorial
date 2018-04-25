ce7985db17a110357fa3c02a673d807e37ee7ec7
robert masen <r@robertmasen.pizza>
2018-04-25 09:26:54 -0500

    fill in batch script for building wasm

--- build_wasm.bat
+++ build_wasm.bat
@@ -1,4 +1,23 @@
-cd crates/browser
+@echo off
+del %~dp0dist\*.wasm
+cd crates\browser
 cargo +nightly build --target wasm32-unknown-unknown
-cd ../..
-wasm-bindgen .\target\wasm32-unknown-unknown\debug\wasm_tutorial_browser.wasm --out-dir ./dist
+cd ..\..
+wasm-bindgen ./target\wasm32-unknown-unknown\debug\wasm_tutorial_browser.wasm --browser --out-dir ./dist
+echo "%~dp0wbch.exe"
+if not exist %~dp0wbch.exe (
+    echo "Cloning wbch into temp directory"
+    git clone https://github.com/freemasen/wasm-chrome-hack %TEMP%\wasm_tut
+    cd "%TEMP%\wasm_tut"
+    cargo build --release
+    cd %~dp0
+    echo "Attempting to copy"
+    robocopy %TEMP%\wasm_tut\target\release %~dp0 wbch.exe
+    echo "Removing temp directory for repo"
+    rmdir /S /Q %TEMP%\wasm_tut
+)
+echo "modifying file"
+rename %~dp0dist\wasm_tutorial_browser_bg.wasm bincode_parse.wasm
+wbch.exe ./dist/wasm_tutorial_browser.js ./ts/wasm_tutorial_browser.js /bincode_parse.wasm
+del /F /Q %~dp0dist\*.js
+%~dp0node_modules\.bin\webpack.cmd %1
