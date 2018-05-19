ac872b3b2634e497e9e7180052039d47c4900094
Robert Masen <r@robertmasen.pizza>
2018-05-19 18:16:29 -0500

    more build

--- build.rs
+++ build.rs
@@ -9,27 +9,23 @@ fn main() {
             .arg("install")
             .arg("nightly")
             .spawn()
-            .expect("Unable to execute nightly install")
             .wait_with_output()
-            .expect("Unable to wai");
+            .expect("Unable to execute nightly install");
         Command::new("~/.cargo/bin/rustup")
             .arg("target")
             .arg("add")
             .arg("wasm32-unknown-unknown")
-            .expect("Unable to execute wasm target")
             .wait_with_output()
-            .expect("unable to wait");
+            .expect("Unable to execute wasm target");
         Command::new("~/.cargo/bin/cargo")
             .arg("install")
             .arg("wasm-bindgen")
-            .expect("Unable to execute install wasm-bindgen")
             .wait_with_output()
-            .expect("Unable to wait")
+            .expect("Unable to execute install wasm-bindgen");
         Command::new("sh")
             .arg("build_wasm.sh")
             .spawn()
-            .expect("Unable to execute build script")
             .wait_with_output()
-            .expect("Unable to wait");
+            .expect("Unable to execute build script");
     }
 }
