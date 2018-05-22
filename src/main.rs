16d9640589c540cff9fd82949a0383a8e4fa338b
robert masen <r@robertmasen.pizza>
2018-05-22 12:45:17 -0500

    ensure correct port binding?

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
+use std::env::{set_var, var};
+
 extern crate wasm_tutorial_server;
 fn main() {
-    wasm_tutorial_server::start_server("8888");
+    let port = match var("PORT") {
+        Ok(port) => {
+            println!("found $PORT env var: {:?}", &port);
+            port
+        },
+        Err(e) => {
+            println!("Unable to get $PORT from env {:?}", e);
+            let port = String::from("8888");
+            set_var("PORT", &port);
+            port
+        },
+    };
+    wasm_tutorial_server::start_server(&port);
 }
