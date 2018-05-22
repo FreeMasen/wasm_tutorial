bd85e100ae5adbc12cc6b1b07623eb533da8db04
robert masen <r@robertmasen.pizza>
2018-05-22 14:16:28 -0500

    port?

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
-use std::env::{set_var, var};
+use std::env::{set_var, var, args};
 
 extern crate wasm_tutorial_server;
 fn main() {
-    let port = match var("PORT") {
-        Ok(port) => {
-            println!("found $PORT env var: {:?}", &port);
-            port
-        },
-        Err(e) => {
-            println!("Unable to get $PORT from env {:?}", e);
-            let port = String::from("8888");
-            set_var("PORT", &port);
-            port
-        },
+    let port = if let Some(p) = args().nth(1) {
+        println!("passed port arg: {}", &p);
+        p
+    } else {
+        match var("PORT") {
+            Ok(port) => {
+                println!("found $PORT env var: {:?}", &port);
+                port
+            },
+            Err(e) => {
+                println!("Unable to get $PORT from env {:?}", e);
+                let port = String::from("8888");
+                set_var("PORT", &port);
+                port
+            },
+        }
     };
     wasm_tutorial_server::start_server(&port);
 }
