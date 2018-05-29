1c81fdeca9b0b237132e8ebf10a05d58c4720b7d
robert masen <r@robertmasen.pizza>
2018-05-29 12:19:39 -0500

    final build update?

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ if [[ ! $CARGOHOME ]]; then
 fi
 if [ ! -f $CARGOHOME/bin/cargo-upstall ]; then
     echo "cargo-install does not exist"
+    cargo install cargo-upstall
 fi
-cargo upstall cargo-upstall 
 echo "Installing nightly"
 rustup install nightly
 echo "Installing wasm32-unknown-unknown"
@@ -19,7 +19,7 @@ echo "building the browser project"
 cargo +nightly build --target wasm32-unknown-unknown
 cd ../..
 echo "updating cargo upstall"
-# cargo upstall cargo-upstall
+cargo upstall cargo-upstall
 echo "installing wasm-bindgen"
 cargo upstall wasm-bindgen-cli
 echo "making sure that rsass exists"
